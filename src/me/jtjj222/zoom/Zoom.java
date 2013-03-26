bca11911e7aa9a5e2a5ac0d569d83a7a4289cfce
lemon42 <webmaster@xylemstudios.com>
2013-03-26 16:11:12 +0100

    Cleaned up the code a bit, optimized some stuff.

--- src/me/jtjj222/zoom/Zoom.java
+++ src/me/jtjj222/zoom/Zoom.java
@@ -36,7 +36,7 @@ public class Zoom extends JavaPlugin implements Listener {
 	//Stores <playername, number of times they have clicked>
 	//If they click, they zoom to the zoomLevels[click_count].
 	public HashMap<String, Integer> playersZoomedIn = new HashMap<String, Integer>();
-		
+	
 	public void onEnable() {
 		//Save default configuration
 		this.saveDefaultConfig();
@@ -47,11 +47,9 @@ public class Zoom extends JavaPlugin implements Listener {
 		
 		Material m = Material.getMaterial(configMagicItem);
 		if (m == null) {
-			try {
-				//ID support
-				m = Material.getMaterial(Integer.parseInt(configMagicItem));
-				if(m == null) throw new Exception(); //Throws exception, goes to error message.
-			} catch (Exception e) {
+			//ID support
+			m = Material.getMaterial(Integer.parseInt(configMagicItem));
+			if(m == null) {
 				getLogger().log(Level.INFO, "Could not find item " + configMagicItem + ". Using SPIDER_EYE instead.");
 				m = Material.SPIDER_EYE;
 			}
@@ -78,18 +76,21 @@ public class Zoom extends JavaPlugin implements Listener {
 		
 		for(Integer i : getConfig().getIntegerList("Zoom_Levels"))
 			if(i >= 0 && i <= 12) zoomLevels.add(i);
-			
+		
 		if(zoomLevels.isEmpty()) {
 			getLogger().info("Could not find any zoom valid zoom levels in the configuration. Filling with default values [3; 8; 10; 12].");
 			zoomLevels.add(3);
 			zoomLevels.add(8);
 			zoomLevels.add(10);
 			zoomLevels.add(12);
+		} else if(zoomLevels.size() == 1 && zoomLevels.get(0) == 0) {
+			getLogger().info("Your zoom levels only contain the value 0. The zoom item will be disabled.");
+			//No need to take extra action, the plugin already knows what to do.
 		}
 		
 		//Message
 		message = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Message"));
-		if(ChatColor.stripColor(message).trim() == "") message = "";
+		if(ChatColor.stripColor(message).trim() == "") message = ""; //In case there's nothing visible :3
 		//Value display
 		valueDisplay = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Value_Display"));
 		
@@ -99,7 +100,7 @@ public class Zoom extends JavaPlugin implements Listener {
 		}
 		
 		//Schedule checking tasks
-		getServer().getScheduler().runTaskTimerAsynchronously(this, new CheckTask(this), 0, 10000);
+		getServer().getScheduler().runTaskTimerAsynchronously(this, new CheckTask(this), 0, 12000);
 		
 		//Up and runnin'!
 		getLogger().info("Version " + getDescription().getVersion() + " enabled!");
@@ -120,7 +121,7 @@ public class Zoom extends JavaPlugin implements Listener {
 				setZoomLevel(p, 0);
 		}
 	}
-
+	
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerQuit(PlayerQuitEvent e) {
 		setZoomLevel(e.getPlayer(), 0);
@@ -140,25 +141,14 @@ public class Zoom extends JavaPlugin implements Listener {
 		Action zoomOutBlockAction = leftMouseButton ? Action.RIGHT_CLICK_BLOCK : Action.LEFT_CLICK_BLOCK;
 		
 		if (e.getMaterial() == magicItem) {
+			//Replaced the big ifs we had, since the setZoomLevel already handled out of bounds cases to match the default behaviour :)
 			if (e.getAction() == zoomInAirAction || e.getAction() == zoomInBlockAction) {
 				//Zoom in!
-				if (playersZoomedIn.containsKey(e.getPlayer().getName())) {
-					//Consequent zoom
-					setZoomLevel(e.getPlayer(), getClicks(e.getPlayer()) + 1);
-				} else {
-					//First zoom
-					setZoomLevel(e.getPlayer(), 1);
-				}
+				setZoomLevel(e.getPlayer(), getClicks(e.getPlayer()) + 1);
 				if(cancelInteract) e.setCancelled(true);
 			} else if(useOtherButton && (e.getAction() == zoomOutAirAction || e.getAction() == zoomOutBlockAction)) {
 				//Zoom out!
-				if (playersZoomedIn.containsKey(e.getPlayer().getName())) {
-					//Consequent zoom
-					setZoomLevel(e.getPlayer(), getClicks(e.getPlayer()) - 1);
-				} else {
-					//Zoom back to maximum level
-					setZoomLevel(e.getPlayer(), zoomLevels.size());
-				}
+				setZoomLevel(e.getPlayer(), getClicks(e.getPlayer()) - 1);
 				if(cancelInteract) e.setCancelled(true);
 			}
 		}
@@ -172,7 +162,7 @@ public class Zoom extends JavaPlugin implements Listener {
 	}
 	void setZoomLevel(Player p, Integer clicks, boolean b) {
 		if(p == null || !p.hasPermission("zoom.use")) return;
-
+		
 		int initialZoom = getClicks(p);
 		
 		if(clicks < 0) clicks = zoomLevels.size();
@@ -182,12 +172,12 @@ public class Zoom extends JavaPlugin implements Listener {
 			clicks = 0;
 			if(playersZoomedIn.containsKey(p.getName())) playersZoomedIn.remove(p.getName());
 		} else {
-			p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20000, zoomLevels.get(clicks - 1)));
+			if(zoomLevels.get(clicks - 1) != 0) p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 24000, zoomLevels.get(clicks - 1)));
 			playersZoomedIn.put(p.getName(), clicks);
 		}
 		
 		int zoomLevel = (clicks == 0 ? 0 : zoomLevels.get(clicks - 1));
 		String level = valueDisplay.replace("%value%", "" + zoomLevel).replace("%index%", "" + clicks).replace("%total%", "" + zoomLevels.size());
-		if(!message.equals("") && b && initialZoom != zoomLevel) p.sendMessage(message.replace("%level%", level));
+		if(b && initialZoom != zoomLevel && !message.equals("")) p.sendMessage(message.replace("%level%", level));
 	}
 }
